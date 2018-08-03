4a0628e6332e360f726410303d5042f286258370
PSLCOL\RGarciaE <rgarciae@psl.com.co>
2018-08-03 11:15:43 -0500

    First working test

--- AndroidCalculatorTest.py
+++ AndroidCalculatorTest.py
@@ -1,3 +1,32 @@
-class AndroidCalculatorTest:
+from appium import webdriver
+import unittest
+
+class AndroidCalculatorTest(unittest.TestCase):
     def setUp(self):
-        desired_caps = {}
+        desired_capabilities = {'automationName': 'Appium',
+                                'platformName': 'Android',
+                                'platformVersion': '7.1.1',
+                                'deviceName': 'Android Emulator',
+                                'appPackage': 'com.android.calculator2',
+                                'appActivity': 'Calculator'}
+        
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities)
+
+    def test_sum(self):
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_1').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_add').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_2').click()
+        self.assertEqual(self.driver.find_element_by_id('com.android.calculator2:id/result').text,'3')
+
+    def test_substraction(self):
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_9').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_7').click()
+        self.assertEqual(self.driver.find_element_by_id('com.android.calculator2:id/result').text,'2')
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(AndroidCalculatorTest)
+    unittest.TextTestRunner(verbosity=6).run(suite)
---
+++ CalculatorPage.py
@@ -0,0 +1,24 @@
+
+class CalculatorPage:
+    digits = {'zero': 'com.android.calculator2:id/digit_0', 
+    'one': 'com.android.calculator2:id/digit_1',
+    'two': 'com.android.calculator2:id/digit_2',
+    'three': 'com.android.calculator2:id/digit_3',
+    'four': 'com.android.calculator2:id/digit_4',
+    'five': 'com.android.calculator2:id/digit_5',
+    'six': 'com.android.calculator2:id/digit_6',
+    'seven': 'com.android.calculator2:id/digit_7',
+    'eight': 'com.android.calculator2:id/digit_8',
+    'nine': 'com.android.calculator2:id/digit_9'}
+    
+    others = {'decimal_separator': 'com.android.calculator2:id/dec_point',
+    'delete': 'com.android.calculator2:id/del',
+    'equals': 'com.android.calculator2:id/eq'}
+
+    operations = {'addition': 'com.android.calculator2:id/op_add',
+    'substraction': 'com.android.calculator2:id/op_sub',
+    'multiplication': 'com.android.calculator2:id/op_mul',
+    'division': 'com.android.calculator2:id/op_div'}
+
+    text_views = {'result':'com.android.calculator2:id/result',
+    'formula':'com.android.calculator2:id/formula'}
